3687b31d788b6b94f90c18f22c70c3c2d634c447
Javad Ebrahim Pour Bura <jepbura@gmail.com>
2023-05-20 12:36:45 +0330

    05/20/2023 => New update

--- gaganode_install.sh
+++ gaganode_install.sh
@@ -2,8 +2,8 @@
 
 cpu_arch="$(uname -m)"
 echo "cpu_arch=$cpu_arch"
-AMD="https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz"
-ARM="https://assets.coreservice.io/public/package/24/app/1.0.3/app-1_0_3.tar.gz"
+AMD="https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz"
+ARM="https://assets.coreservice.io/public/package/61/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz"
 
 if [ "${cpu_arch}" = "x86_64" ]; then
 DOWNLOADLINK=$AMD
@@ -18,31 +18,32 @@ fi
 
 echo "DOWNLOADLINK=$DOWNLOADLINK"
 
-if [ -d ./app-linux* ]; then
-echo "app-linux found" &&
-cd ./app-linux* &&
-sudo ./app service start &&
-sudo ./app status &&
-sudo ./app log &&
+if [ -d ./apphub-linux* ]; then
+echo "apphub-linux found" &&
+cd ./apphub-linux* &&
+sudo ./apphub service start &&
+sudo ./apphub status &&
+sudo ./apphub log &&
 sudo ./apps/gaganode/gaganode log &&
 cat ./apps/gaganode/user_conf/default.toml
 else
-echo "app-linux NOT found."
+echo "apphub-linux NOT found."
 sudo curl -o $FILENAME $DOWNLOADLINK &&
 sudo tar -zxf $FILENAME &&
 sudo rm -f $FILENAME &&
-cd ./app-linux* &&
-sudo ./app service install  &&
-sudo ./app service start &&
-sudo ./app status &&
+cd ./apphub-linux* &&
+sudo ./apphub service remove &&
+sudo ./apphub service install
+sudo ./apphub service start
+sudo ./apphub status &&
 sleep 30 && echo ' ____   ___  _   _ _____
 |  _ \ / _ \| \ | | ____|
 | | | | | | |  \| |  _|
 | |_| | |_| | |\  | |___
 |____/ \___/|_| \_|_____|'
-sudo ./app status && 
+sudo ./apphub status && 
 sudo ./apps/gaganode/gaganode config set --token=$TOKEN &&
-sudo ./app restart &&
+sudo ./apphub restart &&
 sudo ./apps/gaganode/gaganode log
 fi
 
